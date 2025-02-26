eab573d4ae164172136623cb38808d58eafbe9b0
Yiwen Wang <wangyiwenau@gmail.com>
2025-02-26 16:24:29 +1100

    add react router and simple routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
+import AppLayout from "./ui/AppLayout";
+import AuthLayout from "./ui/AuthLayout";
+import About from "./pages/About";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route element={<AppLayout />}>
+        <Route path="about" element={<About />} />
+      </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+
+function AppLayout() {
+  return (
+    <div>
+      applayout
+      <Outlet />
+    </div>
+  );
+}
+
+export default AppLayout;
