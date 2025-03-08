9af25e8bea310cbf3d48effa6e02275a7de6356f
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-08 16:32:43 +1100

    change css of post, display image in post

--- src/ui/Post.jsx
+++ src/ui/Post.jsx
@@ -2,17 +2,12 @@ import { format } from "date-fns";
 
 function Post({ post }) {
   return (
-    <div className="border-2 border-purple-900 flex flex-col gap-1 w-10/12 py-2 px-3">
-      <div className="text-stone-600">
-        Posted by {post.author} on{" "}
-        {format(new Date(post.createdAt), "dd/MM/yyyy")}
+    <div className="border-b border-purple-900 flex flex-col gap-0 w-1/2 py-2 px-3">
+      <div className="text-stone-600 text-xs">
+        XXX - {format(new Date(post.createdAt), "dd/MM/yyyy")}
       </div>
-      <div className="font-bold text-purple-900 text-3xl">{post.title}</div>
-      <div className="flex flex-row gap-2 justify-end text-stone-800">
-        <div>VIEW</div>
-        <div>EDIT</div>
-        <div>DELETE</div>
-      </div>
+      <div className="font-bold text-purple-900 text-xl">{post.title}</div>
+      <img src={post.imageUrl} />
     </div>
   );
 }
