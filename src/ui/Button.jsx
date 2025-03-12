788fb60c55ea79c85d8aa5d26f8d8dbfc6924fc8
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-12 11:13:22 +1100

    more refine on button component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,12 +1,12 @@
 function Button({ variant = "primary", children, ...props }) {
   const baseStyles =
-    "px-4 py-2 text-xl w-fit shadow-2xl capitalize hover:cursor-pointer";
+    "px-3 py-2 font-semibold w-fit shadow-2xl capitalize hover:cursor-pointer transition duration-300";
 
   const variantStyles = {
-    primary: "bg-yellow-500 text-purple-950 rounded",
-    rounded: "bg-yellow-500 text-purple-950 rounded-3xl",
-    warning: "bg-red-300 text-white rounded",
-    logo: "rounded-none font-bold border-2 border-white text-white rounded",
+    primary: "bg-yellow-500 text-purple-950 rounded hover:bg-yellow-600",
+    rounded: "bg-yellow-500 text-purple-950 rounded-3xl hover:bg-yellow-600",
+    warning: "bg-red-500 text-white rounded hover:bg-red-700",
+    logo: "rounded-none font-bold border-2 border-white text-white",
   };
   return (
     <button className={`${baseStyles} ${variantStyles[variant]}`} {...props}>
