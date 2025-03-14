4a51ba2d76738238ec4ab56f46e90739d7836e44
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-14 16:00:13 +1100

    refine css in delete post

--- src/ui/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete.jsx
@@ -15,12 +15,12 @@ function ConfirmDelete({ postId }) {
   };
   return (
     <div className="w-full h-full flex flex-col gap-2 px-6 py-6 pb-4 bg-white shadow-lg rounded-lg">
-      <label className="py-4 mb-2 text-3xl font-bold text-purple-950 border-b-4 border-purple-950">
+      <label className="flex justify-center items-center text-xl font-bold text-purple-950">
         Confirm Delete
       </label>
-      <p className="text-lg">
+      <p className="font-semibold text-gray-700 px-6">
         Are you sure you want to delete this post permanently? This action
-        cannot be undone.
+        cannot be <span className="text-red-500">undone</span>.
       </p>
       <div className="flex justify-end gap-4 pt-4">
         <Button onClick={handleDelete} variant="warning">
