4454d97283dfafa789edeed8ca4676822e0d1c90
Yiwen Wang <wangyiwenau@gmail.com>
2025-03-17 22:32:32 +1100

    add print to test url

--- src/api/apiServices.js
+++ src/api/apiServices.js
@@ -9,6 +9,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const fetchPosts = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/feed/posts`);
+    console.log(`${API_BASE_URL}/feed/posts`);
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
